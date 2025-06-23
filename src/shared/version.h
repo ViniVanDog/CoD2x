ed3ec722a31e539335b94cc6ecaac6b64f5d3a05
eyza <kratas.tom@seznam.cz>
2025-06-23 19:34:50 +0200

    Version 1.4.4.2

--- src/shared/version.h
+++ src/shared/version.h
@@ -1,6 +1,6 @@
 #define APP_VERSION_MAJOR 1 			
 #define APP_VERSION_MINOR 4 			
 #define APP_VERSION_PROTOCOL 4 	
-#define APP_VERSION_PATCH 1 			
+#define APP_VERSION_PATCH 2 			
 #define APP_VERSION_IS_TEST 0 		
-#define APP_VERSION "1.4.4.1" 					
+#define APP_VERSION "1.4.4.2" 					
